f65fe54f22f94ed05cc49b94634910591dfde87b
gsukhanov <gregter1999@gmail.com>
2025-12-30 18:19:35 +0300

    Delete homework2 directory

--- homework2/bank-app/src/main/java/bank/commands/tocsv/ToCsv.java
+++
@@ -1,7 +0,0 @@
-package bank.commands.tocsv;
-
-import bank.commands.interfaces.ICommand;
-
-public abstract class ToCsv implements ICommand {
-    public abstract String execute();
-}
--- homework2/bank-app/src/main/java/bank/commands/tojson/ToJson.java
+++
@@ -1,8 +0,0 @@
-package bank.commands.tojson;
-
-import bank.commands.interfaces.ICommand;
-import org.json.JSONObject;
-
-public abstract class ToJson implements ICommand {
-    public abstract JSONObject execute();
-}
